8c365d6fc1acabc22dd2a3a93500d01ed8921f88
EthanC2 <ethanrcox@gmail.com>
2022-10-22 17:32:58 -0400

    initial commit

---
+++ tests/integration_tests.rs
@@ -0,0 +1,26 @@
+use leetspeak;
+
+#[test]
+fn translate_random() {
+    
+}
+
+#[test]
+fn translate_level1() {
+    
+}
+
+#[test]
+fn translate_level2() {
+    
+}
+
+#[test]
+fn translate_level3() {
+    
+}
+
+#[test]
+fn translate_custom() {
+    
+}
